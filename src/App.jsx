9c386c2c47cb11749bea0211c8522cd663023e33
RaquelWebDev <raquel.torre.oporto@gmail.com>
2023-12-13 18:53:23 +0100

    client created

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Navegation from './components/Navbar'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Navegation />
+
+
+    </>
+  )
+}
+
+export default App
